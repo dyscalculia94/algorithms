3cf379b350c8917e786a3b9f99cf7bcb355b318e
dyscalculia94 <nikola.blagojevic94@yahoo.com>
2019-03-09 11:10:31 +0100

    Added the swap macro

--- inc/helper.h
+++ inc/helper.h
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// TODO: Optimise the swap (really low priority)
+#define SWAP(x, y)	\
+do {				\
+	int temp = (x);	\
+	(x) = (y);		\
+	(y) = temp;		\
+} while (0);		\
+
 void print_array(int32_t *array, size_t len);
 
 #endif
