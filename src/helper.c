f489f80eec17e4dbb3bffa3cbb6eaf4ad34173f9
dyscalculia94 <nikola.blagojevic94@yahoo.com>
2019-03-10 23:19:16 +0100

    Refactored the code a bit

--- src/helper.c
+++ src/helper.c
@@ -3,9 +3,8 @@
 void print_array(int32_t *array, size_t len)
 {
 	printf("%d", array[0]);
-	for (size_t i = 1; i < len; i++) {
+	for (size_t i = 1; i < len; i++)
 		printf(" %d", array[i]);
-	}
 }
 
 int32_t * generate_array(size_t len)
