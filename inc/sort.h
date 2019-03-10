4e4d590ae2aa33716320c0114a42711af57b21ac
dyscalculia94 <nikola.blagojevic94@yahoo.com>
2019-03-10 22:55:46 +0100

    Added the merge sort

--- inc/sort.h
+++ inc/sort.h
@@ -2,11 +2,14 @@
 #define SORT_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include "helper.h"
 
 void insertion_sort(int32_t *array, size_t len);
 
 void selection_sort(int32_t *array, size_t len);
+
+void merge_sort(int32_t *array, size_t start, size_t end);
 
 #endif
--- src/sort.c
+++ src/sort.c
@@ -1,5 +1,45 @@
 #include "sort.h"
 
+/*******************************************************************************
+ *                              PRIVATE FUNCTIONS                              *
+ ******************************************************************************/
+
+static void merge(int32_t *array, size_t p, size_t q, size_t r)
+{
+	size_t n1 = q - p, n2 = r - q;
+	int32_t *arr1 = malloc(n1 * sizeof(int32_t));
+	int32_t *arr2 = malloc(n2 * sizeof(int32_t));
+
+	for (size_t i = 0; i < n1; i++)
+		arr1[i] = array[p + i];
+	for (size_t i = 0; i < n2; i++)
+		arr2[i] = array[q + i];
+
+	size_t j = 0, k = 0;
+	for (size_t i = 0; i < (r - p); i++) {
+		int32_t temp;
+		if (j < n1 && k < n2) {
+			if (arr1[j] <= arr2[k])
+				temp = arr1[j++];
+			else
+				temp = arr2[k++];
+		}
+		else if (j < n1) {
+			temp = arr1[j++];
+		}
+		else
+			temp = arr2[k++];
+		array[p + i] = temp;
+	}
+
+	free(arr2);
+	free(arr1);
+}
+
+/*******************************************************************************
+ *                              PUBLIC FUNCTIONS                               *
+ ******************************************************************************/
+
 void insertion_sort(int32_t *array, size_t len)
 {
 	for (size_t i = 1; i < len; i++) {
@@ -25,4 +65,13 @@ void selection_sort(int32_t *array, size_t len)
 
 		SWAP(array[i], array[idx]);
 	}
+}
+
+void merge_sort(int32_t *array, size_t start, size_t end)
+{
+	if (end - start < 2)
+		return ;
+	merge_sort(array, start, (end + start) / 2);
+	merge_sort(array, (end + start) / 2, end);
+	merge(array, start, (end + start) / 2, end);
 }
