42c867a657a7b4e9264ef0ff4173647d9bcda5a8
dyscalculia94 <nikola.blagojevic94@yahoo.com>
2019-03-11 18:31:08 +0100

    Added the binary search

---
+++ inc/search.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int32_t binary_search(int32_t *array, size_t start, size_t end, int32_t value);
+
+#endif
---
+++ src/search.c
@@ -0,0 +1,16 @@
+#include "search.h"
+
+int32_t binary_search(int32_t *array, size_t start, size_t end, int32_t value)
+{
+	if (end - start == 1)
+		return -1;
+
+	int idx = (end + start) / 2;
+
+	if (array[idx] == value)
+		return idx;
+	else if (array[idx] > value)
+		return binary_search(array, start, idx, value);
+	else
+		return binary_search(array, idx, end, value);
+}
